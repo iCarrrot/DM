ac508d593a151aa15f2a183fcfea661a82955d66
iCarrrot <michal.mar3@gmail.com>
2018-11-04 08:50:40 +0100

    Minor update

--- 2/z2.m
+++ 2/z2.m
@@ -1,3 +1,3 @@
 function Res = z2(X,Y)
-    Res = sqrt(sum(X .* X) - 2 * transpose(Y) * X + transpose(sum(Y .* Y)));
+    Res = sqrt(sum(X .* X) - 2 * Y.' * X + transpose(sum(Y .* Y)));
 end
--- 2/z3.m
+++ 2/z3.m
@@ -1,5 +1,8 @@
 function Res = z3(X,Y,k)
-    Dists = sqrt(sum(X .* X) - 2 * transpose(Y) * X + transpose(sum(Y .* Y)));
+    Dists = sqrt(sum(X .* X) - 2 * Y.' * X + transpose(sum(Y .* Y)));
+    
     [val, indx] = sort(Dists, 2);
+    
     Res = indx(:, 1:k);
+
 end
